8ed6c1edd2b1510c40c6e67e79d15ae2eafd42c1
Dharmik <20it087@charusat.edu.in>
2024-02-20 11:01:07 +0530

    Understanding password hashing, salt and paper. Hashing Passwords Using bcrypt Js.  and implement JWT - it is a method to verify user . jwt is use for safe communication between client and server

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,13 @@ const express = require('express');
 // then import router to use routes 
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'dharmikpatel$cric';
 
+// jwt is use for safe communication between client and server
 /*
  this simmiler to the
     app.get('/', (req, res) => {
@@ -16,6 +20,7 @@ but here we use router.get(....)
 
 // create a user using : Post " /api/auth/createuser" , No Login Required
 router.post('/createuser', [
+    // take it from express validator
     body('name', 'Enter valid name').isLength({ min: 2 }),
     body('email', 'Enter Valid Email please Cheack').isEmail(),
     body('password', 'Your Password is not valid , please Enter valid Password').isLength({ min: 5 })
@@ -26,19 +31,32 @@ router.post('/createuser', [
     }
 
     // check the new created user is already exists or not with the help of email
+    // take it from express validator
     try {
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ errors: "Sorry a user with this email is already exists" });
         }
-
+        // bcrypt js - here we generate salt for more secure password
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         // create a new user 
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         })
-        res.json(user);
+        // res.json(user);
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        console.log(authtoken);
+
+        res.json({ authtoken });
     }
 
     catch (error) {
