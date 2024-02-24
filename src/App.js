08b124c804001186d4fc7978bca8e9b466914ce7
Dharmik <20it087@charusat.edu.in>
2024-02-24 11:49:01 +0530

    Add Bootstrap cass and js links  For use the built in features from the bootstrap.then create component folder and inside this create components. then stepup react router using react-router-dom . and validate the exact path. complete the router setup.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Inotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </Router >
+    </>
   );
 }
 
