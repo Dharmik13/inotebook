d258ba4033de10eb47f5a43f6eb9236487a36866
Dharmik <20it087@charusat.edu.in>
2024-02-26 12:16:17 +0530

    Now We use useLocation Hook For Navbar ,  when we are in home page then in the navbar home is dark and when we are in about the about is active or dark , and we take reference of uselocation hook is from react router web site .... search uselocation hook react router.

--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -2,29 +2,13 @@
 
 // for using the context API we need to imp create Context file 
 import NoteContext from "./noteContext";
-import { useState } from "react";
+
 
 const NoteState = (props) => {
 
-    const s1 = {
-        "name": "Dharmik",
-        "class": "10a"
-    }
 
-    // we can set the state or update the state also for this we need to use useState
-    const [state, setstate] = useState(s1);
-
-    // for update the state 
-    const update = () => {
-        setTimeout(() => {
-            setstate({
-                "name": "DmPatel",
-                "class": "12a"
-            })
-        }, 3000);
-    }
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
